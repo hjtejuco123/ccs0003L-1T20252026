606408fb2b5762caeb0110a57dab964fe61a19d4
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-19 07:39:52 +0800

    Create sample1.cpp

---
+++ Module3/sample1.cpp
@@ -0,0 +1,13 @@
+/******************************************************************************
+*******************************************************************************/
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    cout<<"Hello World";
+    cout<<"\nI Love Programming in c++"<< endl;
+    cout<<"Square root of 16 is = " << sqrt(16) << endl;
+    cout<< "Power 2^3 = "<<pow(2,3) << endl;
+    return 0;
+}
