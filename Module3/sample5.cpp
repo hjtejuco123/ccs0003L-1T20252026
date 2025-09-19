8b958b9979d72c954b72cdb96de6c9ef3c9af5b8
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-19 08:50:42 +0800

    Create sample5.cpp

---
+++ Module3/sample5.cpp
@@ -0,0 +1,16 @@
+/******************************************************************************
+//sizeof
+int, float, double, char, string
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Size of int " << sizeof(int) << " bytes" <<endl;
+    cout << "Size of float " << sizeof(float)<< " bytes"  << endl;
+    cout << "Size of double " << sizeof(double)<< " bytes"  << endl;
+    cout << "Size of char " << sizeof(char)<< " bytes"  << endl;
+    cout << "Size of string " << sizeof(string)<< " bytes"  << endl;
+
+    return 0;
+}
