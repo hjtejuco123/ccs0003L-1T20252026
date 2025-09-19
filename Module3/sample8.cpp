d3cf922843b7941a466b2c92e819547d5b8e5d9b
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-19 09:33:54 +0800

    Create sample8.cpp

---
+++ Module3/sample8.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+Comparison Operators
+==,<,>,<=,>=<!=
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a =10;
+    int b =20;
+    cout << "a == b: " << (a==b) << endl;
+    cout << "a < b: " << (a<b) << endl;
+    cout << "a > b: " << (a>b) << endl;
+    cout << "a <= b: " << (a<=b) << endl;
+    cout << "a >= b: " << (a>=b) << endl;
+    cout << "a != b: " << (a!=b) << endl;
+
+    return 0;
+}
