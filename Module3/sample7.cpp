58dc383154e6b7d71cc9d15fef776d033a341434
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-19 09:28:09 +0800

    Create sample7.cpp

---
+++ Module3/sample7.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+Opeators 
++,-,*,/,%
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 15;
+    int b = 4;
+    cout << "a + b " << (a + b) << endl;
+    cout << "a - b " << (a - b) << endl;
+    cout << "a * b " << (a * b) << endl;
+    cout << "a / b " << (a / b) << endl;
+    cout << "a % b " << (a % b) << endl;
+    a=10;
+    b=5;
+    cout << "a++ " << ++a << endl;
+    cout << "b-- " << --b << endl;
+    
+    return 0;
+}
