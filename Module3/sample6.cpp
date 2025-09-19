c5462cfda3063c771d64bc1530225720819b50ad
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-19 09:19:13 +0800

    Create sample6.cpp

---
+++ Module3/sample6.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Assignment operator  = " << endl;
+    int x = 20;
+    double y = 20.55;
+    char a = 'A';
+
+    cout << "Value of x is " << x << endl;
+    cout << "Value of y is " << y << endl;
+    cout << "Value of a is " << a << endl;
+
+    return 0;
+}
