05f7560078cb91753c397d62567437136de47ed5
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-19 08:43:45 +0800

    Create sample4.cpp

---
+++ Module3/sample4.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+Examples of data types 
+boolean
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool isTrue =false;
+    const double pi = 3.14159;
+    cout << "Boolean " << (isTrue ? "true" :"false")<<endl;
+    cout << "PI = " << pi << endl;
+    // if (isTrue){
+    //     cout << "True";
+    // }else{
+    //     cout << "False";
+    // }
+    return 0;
+}
