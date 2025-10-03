8e0ff2d55162420fc6d9d1936f7c5029649c60e5
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-10-03 08:27:56 +0800

    Create sam10.cpp

---
+++ Module4/sam10.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double result = pow(2, 3); // 2 raised to the power of 3
+    cout << "2^3 = " << result;
+    return 0;
+}
