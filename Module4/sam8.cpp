d3a08b6d6b88daebeb91f0bbd1ab1aa453a36e9d
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-10-03 08:26:51 +0800

    Create sam8.cpp

---
+++ Module4/sam8.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double x = 7.8;
+    int y = (int)x; // Explicit conversion from double to int
+    cout << "y = " << y;
+    return 0;
+}
