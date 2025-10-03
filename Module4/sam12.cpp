fb6122576a1c18f01f9b48c629eb617e47aa442f
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-10-03 08:28:33 +0800

    Create sam12.cpp

---
+++ Module4/sam12.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double num = 2.3;
+    cout << "Ceil of 2.3 = " << ceil(num) << endl;
+    cout << "Floor of 2.3 = " << floor(num);
+    return 0;
+}
