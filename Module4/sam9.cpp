6af3a8d44e0a2bcad0eec245c9cbb8d611ccf34d
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-10-03 08:27:33 +0800

    Create sam9.cpp

---
+++ Module4/sam9.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    double b = 2.5;
+    double result = a + b; // Implicit conversion of int to double
+    cout << "Result = " << result;
+    return 0;
+}
