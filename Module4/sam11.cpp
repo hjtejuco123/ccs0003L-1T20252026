648321facab525a4a16bfd36cb325d6d63cf0445
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-10-03 08:28:18 +0800

    Create sam11.cpp

---
+++ Module4/sam11.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double result = sqrt(16); // Square root of 16
+    cout << "Square root of 16 = " << result;
+    return 0;
+}
